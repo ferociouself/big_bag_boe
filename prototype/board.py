c7d8e399e83088110c6ebea8a7c7dcd332713a6b
ferociouself <jhe3cd@virginia.edu>
2017-10-09 19:21:54 -0400

    Board can have things put on it

---
+++ prototype/board.py
@@ -0,0 +1,71 @@
+from math import ceil
+class Board:
+
+    nothing = " "
+
+    highlight_begin = "\033[1;31m"
+    highlight_end = "\033[0m"
+
+    def __init__(self, width, height):
+        self.board = [[self.nothing for x in range(width)] for y in range(height)]
+        self.width = width
+        self.height = height
+        self.in_a_row = int(ceil(height/2.0)) + 1
+
+    def __str__(self):
+        retStr = ""
+        curRow = 0
+
+        for row in self.board:
+            rowStr = " | ".join(row)
+            retStr += rowStr + "\n"
+            curRow += 1
+            if curRow < self.height:
+                retStr += "-"*len(rowStr) + "\n"
+
+        return retStr
+
+    def place_in_space(self, place, spaceX, spaceY):
+        if not self.board[spaceY][spaceX] == self.nothing:
+            return False
+        self.board[spaceY][spaceX] = place
+        self.check_state()
+        return True
+
+    def check_state(self):
+        pass
+
+    def highlight(self, str):
+        return self.highlight_begin + str + self.highlight_end
+
+    def highlight_row(self, val):
+        retStr = ""
+        curRow = 0
+
+        for row in self.board:
+            rowStr = " | ".join(row)
+            retStr += rowStr + "\n"
+            curRow += 1
+            if curRow < self.height:
+                if curRow - val == 1 or curRow - val == 0:
+                    retStr += self.highlight("-"*len(rowStr)) + "\n"
+                else:
+                    retStr += "-"*len(rowStr) + "\n"
+
+        return retStr
+
+    def highlight_col(self, val):
+        retStr = ""
+        curRow = 0
+
+        for row in self.board:
+            rowStr = " | ".join(row[0:val])
+            rowStr += self.highlight((" | " if not val == 0 else "") + row[val] + (" | " if not val == (len(row)-1) else ""))
+            rowStr +=" | ".join(row[val+1:])
+            rowLen = len(rowStr) - (len(self.highlight_begin) + len(self.highlight_end))
+            retStr += rowStr + "\n"
+            curRow += 1
+            if curRow < self.height:
+                retStr += "-"*rowLen + "\n"
+
+        return retStr
---
+++ prototype/ui.py
@@ -0,0 +1,34 @@
+from board import Board
+
+if __name__ == "__main__":
+    players = input("How many players are playing? ")
+
+    board = Board(players+1, players+1)
+
+    print(board)
+
+    player_index = 0
+    game_over = False
+
+    while not game_over:
+        yConfirm = False
+
+        spaceX = 0
+        spaceY = 0
+
+        while not yConfirm:
+            spaceY = input("Which row would player " + (player_index + 1).__str__() + " like to choose: ") - 1
+            print(board.highlight_row(spaceY))
+            print ("Player " + (player_index + 1).__str__() + " chose row " + (spaceY + 1).__str__())
+            spaceXStr = raw_input("Confirm row by entering a column, or enter \"n\" to enter a new column: ")
+            if spaceXStr.isdigit():
+                spaceX = int(spaceXStr) - 1
+                yConfirm = True
+
+
+
+        board.place_in_space((player_index+1).__str__(), spaceX, spaceY)
+        print(board.highlight_col(spaceX))
+        #print(board)
+
+        player_index = (player_index + 1) % players
